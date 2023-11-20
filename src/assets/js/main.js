317e843065e098035418ac4046effabc1f9a03a8
DESKTOP-KKETDVT\NEX006 <ysmarco12@naver.com>
2023-11-20 11:33:05 +0900

    레이아웃 완성

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,11 @@
-console.log('시작하기')
+import { menu } from "./menu";
+import { port } from "./port";
+import { link } from "./link";
+import { smooth } from "./smooth";
+
+window.addEventListener("load", function() {
+  smooth();
+  link();
+  menu();
+  port();
+})
